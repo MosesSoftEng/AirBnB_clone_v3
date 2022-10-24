3ccc0bef68be2b9bc8392c8a377432d51e64face
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-24 20:26:06 +0300

    Add tests for count method

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -93,3 +93,12 @@ test_db_storage.py'])
         s.save()
         self.assertIs(s, models.storage.get(State, s.id))
         self.assertIs(None, models.storage.get(State, "fake_id"))
+
+    def test_count_method(self):
+        """Tests for count method"""
+        self.assertEqual(models.storage.count(State),
+                         len(models.storage.all(State)))
+        current = models.storage.count(State)
+        s = State(name="Texas")
+        s.save()
+        self.assertEqual(models.storage.count(State), current + 1)
