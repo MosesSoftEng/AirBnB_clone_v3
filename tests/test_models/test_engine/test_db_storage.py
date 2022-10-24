5d209b67653993d9596165b8419d73eaf6ae0eb7
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-24 20:25:49 +0300

    Add tests for get method

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,10 @@ test_db_storage.py'])
 #     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
 #     def test_save(self):
 #         """Test that save properly saves objects to file.json"""
+
+    def test_get_method(self):
+        """Tests for get method."""
+        s = State(name="Florida")
+        s.save()
+        self.assertIs(s, models.storage.get(State, s.id))
+        self.assertIs(None, models.storage.get(State, "fake_id"))
