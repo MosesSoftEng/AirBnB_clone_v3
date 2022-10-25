7b1b2a8f21e05a1ac5d8d76b967cea9fc855849e
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-25 09:58:04 +0300

    Change from json.dumps to flask jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Define blueprint routes"""
 from api.v1.views import app_views
-import json
+from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
 def api_status():
     """Return status ok json"""
-    return json.dumps({"status": "OK"})
+    return jsonify({"status": "OK"})
