f0f281742d110070c27e8fd4105e9bbf21e7ddaf
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-25 09:42:02 +0300

    Register app_views blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """App API version 1 using flask"""
 from flask import Flask
+from models import storage
+from api.v1.views import app_views
 from os import getenv
 # Create a variable app, instance of Flask.
 app = Flask(__name__)
 
+# Register blueprint app_views
+app.register_blueprint(app_views)
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
