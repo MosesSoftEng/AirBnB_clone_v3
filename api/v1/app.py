ef0ae656dc4b6fa9c94767da8168127b55604b82
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-25 21:41:53 +0300

    Add 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import jsonify
 
 # Create a variable app, instance of Flask.
 app = Flask(__name__)
@@ -18,8 +19,15 @@ def teardown_appcontext(exception):
     """Close storage"""
     storage.close()
 
+
+def page_not_found(e):
+    return jsonify({'error': "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
+
+    app.register_error_handler(404, page_not_found)
 
     app.run(host=host, port=port, threaded=True)
