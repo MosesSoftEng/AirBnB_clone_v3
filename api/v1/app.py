b9d05afdea5e0a9f7e054e75814b974ec7839550
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-25 09:41:20 +0300

    Add flask app run environment varaible

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """App API version 1 using flask"""
 from flask import Flask
+from os import getenv
 # Create a variable app, instance of Flask.
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+
+    app.run(host=host, port=port, threaded=True)
