57e8b71b0e10f8c463d65bb8b2a6620cdcc1d26b
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-26 14:37:53 +0300

    Add post method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,11 +3,25 @@
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
+from flask import request
+from models.state import State
 
 
-@app_views.route('/states', strict_slashes=False)
+@app_views.route(
+    '/states',
+    methods=['GET', 'POST'],
+    strict_slashes=False
+)
 def get_states():
     """Return list of all states as json"""
-    return jsonify(
-        [obj.to_dict() for obj in storage.all('State').values()]
-    )
+    if request.method == 'GET':
+        return jsonify(
+            [obj.to_dict() for obj in storage.all('State').values()]
+        )
+    else:
+        data = request.get_json();
+
+        state = State(**data)
+
+        print(state)
+        return jsonify({})
